e4eb889595023252eb77074e774f3ea4985090cd
jeunju528_sudo <228353885+jeunju528-sudo@users.noreply.github.com>
2026-08-14 14:44:05 +0900

    feat: 상세보기

--- src/main/java/com/sist/web/controller/RouterController.java
+++ src/main/java/com/sist/web/controller/RouterController.java
@@ -3,12 +3,22 @@ package com.sist.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RouterController {
 	@GetMapping("/")
 	public String main_main(Model model) {
 		model.addAttribute("main_html", "main/home");
+		return "main/main";
+	}
+	@GetMapping("/food/detail/{no}")
+	public String food_detail(@PathVariable(value = "no") int no, Model model) {
+		System.out.println("no :: " + no);
+		
+		model.addAttribute("no", no);
+		model.addAttribute("main_html", "food/detail");
+		
 		return "main/main";
 	}
 }
